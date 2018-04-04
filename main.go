efdf2e391a710bb860df03d09bdcba382d1289ac
balkan guler <balkanguler@balkans-MacBook-Pro.local>
2018-04-04 22:49:17 +0300

    cors enabled

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import "github.com/gin-gonic/gin"
 import "net/http"
+import "github.com/gin-contrib/cors"
 
 func main() {
 	router := gin.Default()
-
+	router.Use(cors.Default())
 	router.POST("/concats", func(c *gin.Context) {
 
 		firstname := c.PostForm("firstname")
